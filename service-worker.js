2cd834b5a21ff1300b397b0f90638322282d013f
Sam Van Royen <studiosampersand@users.noreply.github.com>
2026-07-07 23:05:29 +0200

    v0.000.003 openmaps & pushnotif
    final V3 release met openmaps en cloudflare pushnotifications

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='vialego-v0-000-003-final-launch';
+const CACHE='vialego-v0-000-003-ors-release';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
