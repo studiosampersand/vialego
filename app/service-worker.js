82ba3e227c1a8eccaaa2d659c6af51d5ac4175e9
Sam Van Royen <studiosampersand@users.noreply.github.com>
2026-07-09 22:53:47 +0200

    Add files via upload

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='mobud-production-v0-000-009-responsive-2';
-const V='0092';
+const CACHE='mobud-production-v0-000-009-light-chevron-hotfix6';
+const V='0096';
 const ASSETS=['./','./index.html','./styles.css?v='+V,'./app.js?v='+V,'./config.js?v='+V,'./i18n.js?v='+V,'./manifest.json','./content/faq.json','./content/mobility-providers.json','./faq/index.html','./faq/faq.css?v='+V,'./faq/faq.js?v='+V,'./icon-192-v005.png','./icon-512-v005.png','./icon-maskable-512-v005.png'];
 const STATIC_PATHS=new Set(ASSETS.map(x=>new URL(x,self.registration.scope).pathname));
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
