10b6a0a3c9e8dfdf074caf4d07dc70b659135a6c
Sam Van Royen <studiosampersand@users.noreply.github.com>
2026-07-10 21:58:18 +0200

    pdf fixes voor rapportage

--- beta-test/service-worker.js
+++ beta-test/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mobud-beta-v0-000-0105-vehicle-form-spacing';
+const CACHE='mobud-beta-v0.000.010-final3';
 const V='0105';
 const ASSETS=['./','./index.html','./styles.css?v='+V,'./app.js?v='+V,'./config.js?v='+V,'./i18n.js?v='+V,'./pdf-lib.min.js?v='+V,'./manifest.json','./content/faq.json','./content/mobility-providers.json','./faq/index.html','./faq/faq.css?v='+V,'./faq/faq.js?v='+V,'./icon-192-v005.png','./icon-512-v005.png','./icon-maskable-512-v005.png'];
 const STATIC_PATHS=new Set(ASSETS.map(x=>new URL(x,self.registration.scope).pathname));
